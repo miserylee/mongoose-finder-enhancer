306cee4a185f0c292aaf2a4bdf5eb769698c0763
Misery Lee <miserylee@foxmail.com>
2018-11-16 14:53:58 +0800

    Update mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,52 @@
-export default function sayHello(to: string) {
-  return `Hello, ${to}`;
+import { Document, Query, Schema } from 'mongoose';
+
+interface IQuery extends Query<Document> {
+  options: { [option: string]: any };
+  _fields: { [field: string]: number | string } | undefined;
+  schema: {
+    paths: {
+      [path: string]: any;
+    };
+  };
 }
+
+export = (schema: Schema) => {
+  const callback = function(this: IQuery, next: () => void) {
+    console.log('Hint', typeof this);
+    if (this.options.disableEnhancer) {
+      next();
+      return;
+    }
+    if (!this._fields) {
+      this.select('_id');
+    }
+    const fields = Object.keys(this._fields!);
+    const isInclude = this._fields![fields[0]] !== 0;
+    Object.keys(this.schema.paths).filter(key => {
+      return isInclude === fields.includes(key);
+    }).forEach(key => {
+      const ref = this.schema.paths[key].options.ref;
+      if (ref) {
+        let select = this._fields![key];
+        if (typeof select !== 'string') {
+          select = '_id';
+        }
+        this.populate({
+          path: key,
+          model: ref(),
+          select,
+        });
+      }
+    });
+    next();
+  };
+
+  schema.pre<IQuery>('count', callback);
+  schema.pre<IQuery>('find', callback);
+  schema.pre<IQuery>('findOne', callback);
+  schema.pre<IQuery>('findOneAndRemove', callback);
+  schema.pre<IQuery>('findOneAndUpdate', callback);
+  schema.pre<IQuery>('update', callback);
+  schema.pre<IQuery>('updateOne', callback);
+  schema.pre<IQuery>('updateMany', callback);
+};
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,8 +1,27 @@
 import * as assert from 'assert';
-import sayHello from '../src';
+import * as mongoose from 'mongoose';
+import { Schema } from 'mongoose';
 
-describe('Say hello', () => {
-  it('to world should ok', () => {
-    assert(sayHello('world') === 'Hello, world');
+const schema = new Schema({
+  name: String,
+});
+
+schema.plugin(require('../src/index'));
+
+const connection = mongoose.createConnection('mongodb://localhost/test');
+const User = connection.model('user', schema);
+
+describe('mongoose-finder-enhancer', () => {
+  it('should ok', async () => {
+    await User.create({ name: 'Misery' });
+    const user = await User.findOne();
+    if (user) {
+      assert(Object.keys(user.toJSON()).length === 1);
+      await user.remove();
+    }
   });
+});
+
+after(async () => {
+  await connection.close();
 });
